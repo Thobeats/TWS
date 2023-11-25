fbd26621a686f9d6c4e1878c87fe61ae3eb0b5b5
Thobeats <tobiy23@gmail.com>
2023-11-25 17:32:36 +0100

    update

--- resources/views/livewire/just-in.blade.php
+++ resources/views/livewire/just-in.blade.php
@@ -49,7 +49,7 @@
                                                 {{ $new->vendorName() }}
                                             </a>
                                             <a href="/market/product/{{$new->id}}" class="stext-104 cl4 hov-cl1 trans-04 js-name-b2">
-                                                {{ $new->name }}
+                                                {{ substr($new->name,0,10) }}
                                             </a>
                                        </div>
 
